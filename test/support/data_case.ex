f3a0262ad63b0d5d3658a2075104f43e16b246ac
João Victor <vitox320@gmail.com>
2023-07-24 23:50:04 -0300

    Add security and lint configuration on project

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -15,7 +15,7 @@ defmodule PipelineCi.DataCase do
   """
 
   use ExUnit.CaseTemplate
-
+  alias Ecto.Adapters.SQL.Sandbox
   using do
     quote do
       alias PipelineCi.Repo
@@ -36,8 +36,8 @@ defmodule PipelineCi.DataCase do
   Sets up the sandbox based on the test tags.
   """
   def setup_sandbox(tags) do
-    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(PipelineCi.Repo, shared: not tags[:async])
-    on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
+    pid = Sandbox.start_owner!(PipelineCi.Repo, shared: not tags[:async])
+    on_exit(fn -> Sandbox.stop_owner(pid) end)
   end
 
   @doc """
